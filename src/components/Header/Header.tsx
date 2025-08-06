dd4ad9307d7792967ab3549744d3eade83435f55
Muhammed Hesham <muhammedheshamm1@gmail.com>
2025-08-06 18:25:40 +0300

    feat: global styles and header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,12 @@ import './Header.scss'
 
 function Header() {
   return (
-    <div className="Header">Header</div>
+     <header className="header">
+      <div className="container">
+        <h1 className="header__title">Cell Tower Dashboard</h1>
+        <p className="header__subtitle">Monitor cell tower operations across all cities</p>
+      </div>
+    </header>
   )
 }
 
